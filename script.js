b1bdd3ff9b3b64ba7be6398547c5df1113f43ddd
Sebbydoh <sebbydoh@gmail.com>
2020-03-08 23:06:13 -0700

    Defined variables containing each square identified by ID

---
+++ script.js
@@ -0,0 +1,10 @@
+//Game Squares
+let a1 = document.getElementById("square-0");
+let a2 = document.getElementById("square-1");
+let a3 = document.getElementById("square-2");
+let b1 = document.getElementById("square-3");
+let b2 = document.getElementById("square-4");
+let b3 = document.getElementById("square-5");
+let c1 = document.getElementById("square-6");
+let c2 = document.getElementById("square-7");
+let c3 = document.getElementById("square-8");
