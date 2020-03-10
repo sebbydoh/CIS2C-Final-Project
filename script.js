41029bb8c2310582d5859a4041db1d0175c2e118
Sebbydoh <sebbydoh@gmail.com>
2020-03-09 19:05:37 -0700

    creating game play function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Game Board
+let gameBoard = document.getElementById("game-board");
 //Game Squares
 let a1 = document.getElementById("square-0");
 let a2 = document.getElementById("square-1");
@@ -8,3 +10,15 @@ let b3 = document.getElementById("square-5");
 let c1 = document.getElementById("square-6");
 let c2 = document.getElementById("square-7");
 let c3 = document.getElementById("square-8");
+//Current Turn
+let currentTurn = document.getElementById("turn");
+let currentTurnText = currentTurn.value;
+let xTurn = "X";
+let oTurn = "O";
+
+
+function gamePlay(){
+    
+}
+
+gameBoard.addEventListener("click", gamePlay)
